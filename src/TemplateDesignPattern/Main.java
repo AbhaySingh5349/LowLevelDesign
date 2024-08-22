28b71f2f83c7c6920056ff0d5d455916c958a202
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-08-22 16:41:52 +0530

    TemplateDesignPattern

---
+++ src/TemplateDesignPattern/Main.java
@@ -0,0 +1,13 @@
+package TemplateDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        PaymentFlow friendFlow = new PayFriend();
+        friendFlow.sendMoney();
+
+        System.out.println("*****");
+
+        PaymentFlow merchantFlow = new PayMerchant();
+        merchantFlow.sendMoney();
+    }
+}
---
+++ src/TemplateDesignPattern/PaymentFlow.java
@@ -0,0 +1,16 @@
+package TemplateDesignPattern;
+
+public abstract class PaymentFlow {
+    public abstract void validateRequest();
+    public abstract void calculateFee();
+    public abstract void debitAmount();
+    public abstract void creditAmount();
+
+    // template method to define order of execution of task
+    public final void sendMoney(){
+        validateRequest();
+        calculateFee();
+        debitAmount();
+        creditAmount();
+    }
+}
