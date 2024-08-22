363b455e4fa2bd57fbf521025aa57137209b3b17
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-08-22 15:22:54 +0530

    CommandDesignPattern

---
+++ src/CommandDesignPattern/Main.java
@@ -0,0 +1,14 @@
+package CommandDesignPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        AirConditioner airConditioner = new AirConditioner();
+
+        RemoteControlInvoker remoteControlInvoker = new RemoteControlInvoker();
+
+        remoteControlInvoker.setCommand(new TurnOnAcCommand(airConditioner));
+        remoteControlInvoker.pressButton();
+
+        remoteControlInvoker.undo();
+    }
+}
---
+++ src/CommandDesignPattern/TurnOffAcCommand.java
@@ -0,0 +1,19 @@
+package CommandDesignPattern;
+
+public class TurnOffAcCommand implements ICommand{
+    AirConditioner airConditioner;
+
+    public TurnOffAcCommand(AirConditioner airConditioner) {
+        this.airConditioner = airConditioner;
+    }
+
+    @Override
+    public void execute() {
+        airConditioner.turnOffAC();
+    }
+
+    @Override
+    public void undo() {
+        airConditioner.turnOnAC();
+    }
+}
