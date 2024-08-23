e12c292e5040c5d6592cee99adf1fc60d6441496
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-08-23 18:45:10 +0530

    DecoratorDesignPattern

---
+++ src/DecoratorDesignPattern/GiftWrapDecorator.java
@@ -0,0 +1,20 @@
+package DecoratorDesignPattern;
+
+public class GiftWrapDecorator extends ProductDecorator {
+    private double wrapCost = 5.00;
+
+    public GiftWrapDecorator(Product decoratedProduct) {
+        super(decoratedProduct);
+    }
+
+    @Override
+    public String getName() {
+        return decoratedProduct.getName() + " (Gift Wrapped)";
+    }
+
+    @Override
+    public double getPrice() {
+        return decoratedProduct.getPrice() + wrapCost;
+    }
+}
+
---
+++ src/DecoratorDesignPattern/ProductDecorator.java
@@ -0,0 +1,15 @@
+package DecoratorDesignPattern;
+
+public abstract class ProductDecorator extends Product {
+    protected Product decoratedProduct;
+
+    public ProductDecorator(Product decoratedProduct) {
+        super(decoratedProduct.getName(), decoratedProduct.getPrice());
+        this.decoratedProduct = decoratedProduct;
+    }
+
+    @Override
+    public abstract String getName();
+    @Override
+    public abstract double getPrice();
+}
